c16a51c8e8fcac566651ce75b0d52094e8b799e5
abrosimov-d <153320622+abrosimov-d@users.noreply.github.com>
2024-10-01 18:48:14 +0300

    open_file_dialog, save_file_dialog functions

--- filedialog.py
+++ filedialog.py
@@ -1,9 +1,16 @@
+from datetime import datetime
+
 def open_file_dialog():
     directory = input('enter directory: ')
     filenames = input('enter filename, or filenames separated space: ').split(' ')
     result = [f'{directory}\{filename}' for filename in filenames]        
     return result
 
-def save_file_dialog(files):
+def save_file_dialog(filenames):
     result = []
+    directory = input('enter directory: ')
+    for filename in filenames:
+        savefilename = input(f'enter filename for save {filename}:')
+        timestamp = datetime.now().strftime("%d.%m-%H.%M")
+        result.append(f'{directory}\{savefilename}-{timestamp}.png')
     return result

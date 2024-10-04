6f1d1da5c14c8f5d93650d6b706cb0f1d167e760
abrosimov-d <153320622+abrosimov-d@users.noreply.github.com>
2024-10-04 22:48:00 +0300

    add: rotate function

--- actions/image/rotate.py
+++ actions/image/rotate.py
@@ -1,5 +1,27 @@
+from cv2 import getRotationMatrix2D, warpAffine, rotate, ROTATE_90_CLOCKWISE, ROTATE_180, ROTATE_90_COUNTERCLOCKWISE
+
 def rotate_image(app):
-    print('TODO: rotate image ')        
+    error = None
+    try:
+        if 'images' not in app:
+            app['open_action']['function'](app)
+
+        side = input('input rotate side (0: 90-degree, 1: 180-degree, 2: 270-degree, empty: 90-degree): ')
+        if side == '':
+            side = 0
+        else:
+            side = int(side)
+
+        sides = [ROTATE_90_CLOCKWISE, ROTATE_180, ROTATE_90_COUNTERCLOCKWISE]
+        side = sides[side]
+
+        app['images'] = [rotate(image, side) for image in app['images']]
+        
+        app['changes'] = True
+
+    except Exception as e:
+        error = e
+    return error  
 
 action = {
     'name': 'rotate image(s)',

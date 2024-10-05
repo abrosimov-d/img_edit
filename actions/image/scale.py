fb7b96379eb3cca2c1563e5e93946c6370417a3d
abrosimov-d <153320622+abrosimov-d@users.noreply.github.com>
2024-10-05 10:11:18 +0300

    add: scale function

--- actions/image/scale.py
+++ actions/image/scale.py
@@ -1,5 +1,29 @@
+from cv2 import resize
+
 def scale_image(app):
-    print('TODO: scale image ')
+    error = None
+    try:
+        if 'images' not in app:
+            app['open_action']['function'](app)
+
+        value = input('input new size in px for long side (ex. 2000px) or scale factor in % (ex. 150%), (empty for 150%): ').replace(' ', '')
+        if value == '':
+            value = '150%'
+        
+        if value.find('px') > 0:
+            print('px')
+        elif value.find('%') > 0:
+            print('%')
+        else:
+            error = f'wrong input: {value}'
+
+        app['changes'] = True
+    
+    except Exception as e:
+        error = e
+    
+    return error
+
     #return cv2.resize(image, None, fx=scale, fy=scale)
 
 action = {
@@ -7,7 +31,6 @@ action = {
     'function': scale_image,
     'hotkey': '3'
 }
-
 
 def register(app):
     app['actions'].append(action)

f7ceac157ab746fd4269184979783aefad11ca17
abrosimov-d <153320622+abrosimov-d@users.noreply.github.com>
2024-10-08 11:04:47 +0300

    add: sharp image function

--- actions/image/sharp.py
+++ actions/image/sharp.py
@@ -1,12 +1,24 @@
-from cv2 import filter2D, UMat
+from cv2 import filter2D, UMat, CV_32F
+from numpy import array
 
 def sharp_image(app):
-    print('TODO: sharp image')
-    return
-    kernel = [[0, -1, 0], 
-              [-1, 5, -1], 
-              [0, -1, 0]]
-    app['images'] = [filter2D(image, -1, UMat(kernel)) for image in app['images']]
+    
+    error = None
+    
+    try:
+        if 'images' not in app:
+            error = app['open_action']['function'](app)
+            if error != None:
+                return error
+
+        kernel = array([[0, -1, 0], [-1, 5,-1],[0, -1, 0]])
+
+        app['images'] = [filter2D(image, -1, kernel) for image in app['images']]
+
+    except Exception as e:
+        error = e
+    
+    return error
 
 action = {
     'name': 'sharp image(s)',
